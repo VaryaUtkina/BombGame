e3ca0e2baec6faa407d7c030a0717a0a8f88709a
Varvara Utkina <varya2412@icloud.com>
2025-02-09 19:50:39 +0300

    Initial Commit

---
+++ BombGame/BombGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BombGameApp.swift
+//  BombGame
+//
+//  Created by Варвара Уткина on 09.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct BombGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
