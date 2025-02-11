ed964da37fb284206d18553bea502f7a3eea0347
nikita <nikmosyl@gmail.com>
2025-02-11 14:06:18 +0400

    add category view

---
+++ BombGame/CategoryScreen/CategoryView.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryView.swift
+//  BombGame
+//
+//  Created by nikita on 11.02.25.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    
+    
+    var body: some View {
+        Image("Background")
+            .resizable()
+            .ignoresSafeArea(edges: .all)
+    }
+}
+
+#Preview {
+    CategoryView()
+}
