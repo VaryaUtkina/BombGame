d798406a0b642bae405f4dc58df318438183ab43
nikita <nikmosyl@gmail.com>
2025-02-11 14:03:52 +0400

    add Category module

---
+++ BombGame/CategoryScreen/TestCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  TestCategoryView.swift
+//  BombGame
+//
+//  Created by nikita on 11.02.25.
+//
+
+import SwiftUI
+
+struct TestCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestCategoryView()
+}
