ea622650247e667b3297c65956446a103977c05b
SergeyZakurakin <pricezak@gmail.com>
2025-02-11 22:16:26 -0500

    fix font

--- BombGame/MainView/MainView.swift
+++ BombGame/MainView/MainView.swift
@@ -9,23 +9,20 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        
-        
         ZStack {
             Color(red: 255/255, green: 250/255, blue: 94/255)
             Image(.mainbackgroundShape)
                 .resizable()
 
-            
             VStack(spacing: 70) {
                 VStack {
                     Text("ИГРА ДЛЯ КОМПАНИИ")
                         .font(Font.customFont(size: 28).weight(.black))
-                        .foregroundStyle(Colors.TextColors.primary)
                     Text("БОМБА")
                         .font(Font.customFont(size: 48).weight(.black))
-                        .foregroundStyle(Colors.TextColors.primary)
                 }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .foregroundStyle(Colors.TextColors.primary)
                 
                 Image(.mainbomb)
                     .resizable()
@@ -41,14 +38,11 @@ struct MainView: View {
                         .onTapGesture {
                             print("tap2")
                         }
-                        
                 }
                 .padding(.horizontal, 23)
             }
         }
         .ignoresSafeArea()
-        
-        
     }
 }
 
