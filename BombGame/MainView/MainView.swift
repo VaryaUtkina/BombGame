90d2b14e24450b12c57e033ddfce67d1a6ababce
SergeyZakurakin <pricezak@gmail.com>
2025-02-11 22:08:47 -0500

    create mainView

--- BombGame/MainView/MainView.swift
+++ BombGame/MainView/MainView.swift
@@ -9,17 +9,65 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(Colors.ComponentsColors.mainBackground)
-            Text("Hello, world!")
-                .font(Font.customFont(size: 50).bold())
+        
+        
+        ZStack {
+            Color(red: 255/255, green: 250/255, blue: 94/255)
+            Image(.mainbackgroundShape)
+                .resizable()
+
+            
+            VStack(spacing: 70) {
+                VStack {
+                    Text("ИГРА ДЛЯ КОМПАНИИ")
+                        .font(Font.customFont(size: 28).weight(.black))
+                        .foregroundStyle(Colors.TextColors.primary)
+                    Text("БОМБА")
+                        .font(Font.customFont(size: 48).weight(.black))
+                        .foregroundStyle(Colors.TextColors.primary)
+                }
+                
+                Image(.mainbomb)
+                    .resizable()
+                    .frame(width: 274, height: 300)
+                
+                VStack {
+                    ButtonView(text: "Старт игры")
+                        .onTapGesture {
+                            print("tap1")
+                        }
+                    
+                    ButtonView(text: "Категории")
+                        .onTapGesture {
+                            print("tap2")
+                        }
+                        
+                }
+                .padding(.horizontal, 23)
+            }
         }
-        .padding()
+        .ignoresSafeArea()
+        
+        
     }
 }
 
 #Preview {
     MainView()
+}
+
+struct ButtonView: View {
+    var text: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 15)
+            .fill(.mainSheetBg)
+            .frame(height: 55)
+            .overlay {
+                Text(text)
+                    .font(Font.customFont(size: 20).weight(.medium))
+                    .foregroundStyle(Colors.TextColors.primary)
+            }
+         
+    }
 }
