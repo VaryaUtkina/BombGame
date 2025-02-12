a2dc9ebe89aba71e129da62762396628c29646aa
Varvara Utkina <varya2412@icloud.com>
2025-02-12 16:33:10 +0300

    Resource: create CustomButtomView

---
+++ BombGame/Resources/CustomButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  CustomButtonView.swift
+//  BombGame
+//
+//  Created by Варвара Уткина on 12.02.2025.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        VStack {
+        Button {
+            action()
+        } label: {
+            Text(title)
+                .font(Font.customFont(size: 20).weight(.bold))
+                .foregroundStyle(Colors.TextColors.primary)
+        }
+        .frame(maxWidth: .infinity, minHeight: 55)
+        .background(Colors.ComponentsColors.gameViewButton)
+        .clipShape(.rect(cornerRadius: 10))
+        .shadow(radius: 2)
+        .shadow(radius: 2)
+    }
+        .padding(.horizontal, 22.5)
+    }
+}
+
+#Preview {
+    CustomButtonView(title: "Запустить", action: {})
+}
