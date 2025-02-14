72bd9910b7757ea3166ac1ef2dd05df949051af6
nikita <nikmosyl@gmail.com>
2025-02-14 23:39:41 +0400

    add settings manager

--- BombGame/Services/SettingsManager.swift
+++ BombGame/Services/SettingsManager.swift
@@ -10,7 +10,35 @@ import Foundation
 final class SettingsManager {
     static let shared = SettingsManager()
 
+    private var settings = DataManager.shared.getSettings()
+    
     init() {}
     
+    func getSettings() -> Settings {
+        settings
+    }
     
+    func setGameTime(_ time: Settings.GameTime) {
+        settings.gameTime = time
+    }
+    
+    func setBackgroundMusic(_ music: Settings.BackgroundMusic) {
+        settings.backgroundMusic = music
+    }
+    
+    func setTickMusic(_ music: Settings.TickMusic) {
+        settings.tickMusic = music
+    }
+    
+    func setExplosionMusic(_ music: Settings.ExplosionMusic) {
+        settings.explosionMusic = music
+    }
+    
+    func enableVibration(_ enabled: Bool) {
+        settings.vibrationEnable = enabled
+    }
+    
+    func enablePunishments(_ enabled: Bool) {
+        settings.punishmentsEnable = enabled
+    }
 }
