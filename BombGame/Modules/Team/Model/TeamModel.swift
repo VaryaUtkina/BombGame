dbe61da5994178c7803adb11437bc1244c6bfd9a
Varvara Utkina <varya2412@icloud.com>
2025-02-24 13:45:53 +0300

    Create TeamModel

---
+++ BombGame/Modules/Team/Model/TeamModel.swift
@@ -0,0 +1,16 @@
+//
+//  TeamModel.swift
+//  BombGame
+//
+//  Created by Варвара Уткина on 23.02.2025.
+//
+
+import Foundation
+
+struct TeamModel {
+    let text: TeamTexts
+    
+    init() {
+        text = TeamTexts()
+    }
+}
---
+++ BombGame/Modules/Team/Model/TeamTexts.swift
@@ -0,0 +1,16 @@
+//
+//  TeamTexts.swift
+//  BombGame
+//
+//  Created by Варвара Уткина on 23.02.2025.
+//
+
+import Foundation
+
+struct TeamTexts {
+    let navTitle: String
+    
+    init() {
+        navTitle = "Создатели игры"
+    }
+}
