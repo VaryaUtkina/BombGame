9fa06e34c84f922ea21135f86fa5c840889193ce
nikita <nikmosyl@gmail.com>
2025-02-26 18:43:31 +0400

    fix navBar

--- BombGame/Modules/OwnQuestions/View/OwnQuestionsView.swift
+++ BombGame/Modules/OwnQuestions/View/OwnQuestionsView.swift
@@ -13,50 +13,69 @@ struct OwnQuestionsView: View {
     @StateObject private var viewModel = OwnQuestionsViewModel(model: OwnQuestionsModel())
     
     var body: some View {
-        BackgroundView {
-            ScrollView {
+        ZStack {
+            BackgroundView {
                 VStack {
-                    ForEach(viewModel.questions, id: \.self) { question in
-                        OwnQuestionView(
-                            text: question,
-                            onDelete: {
-                                withAnimation { viewModel.deleteQuestion(question) }
+                    ScrollView {
+                        VStack {
+                            ForEach(viewModel.questions, id: \.self) { question in
+                                OwnQuestionView(
+                                    text: question,
+                                    onDelete: {
+                                        withAnimation { viewModel.deleteQuestion(question) }
+                                    }
+                                )
+                                .padding(.top, 8)
                             }
-                        )
-                        .padding(.top, 8)
+                            .padding(.horizontal, 16)
+                        }
+                        .padding(.top, getTopPadding())
+                        .padding(.bottom, 60)
                     }
-                    .padding(.horizontal, 16)
                 }
-                .padding(.top, getTopPadding())
+            }
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: { dismiss() }) {
+                        Image(systemName: "chevron.left")
+                            .resizable()
+                            .frame(width: 12, height: 20)
+                            .foregroundStyle(Color.primaryText)
+                            .font(.title.weight(.bold))
+                    }
+                }
+                ToolbarItem(placement: .principal) {
+                    Text(viewModel.model.texts.title)
+                        .font(Font.customFont(size: 30).weight(.black))
+                        .foregroundStyle(Color.primaryText)
+                        .minimumScaleFactor(0.8)
+                }
             }
             
-            OwnTextField(
-                text: $viewModel.inputText,
-                placeholder: viewModel.model.texts.placeholder,
-                maxCount: viewModel.maximumInputTextCount,
-                onSubmit: {
-                    viewModel.saveQuestion()
-                }
-            )
-            .padding(.horizontal, 16)
-            .padding(.vertical, 8)
-        }
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button(action: { dismiss() }) {
-                    Image(systemName: "chevron.left")
-                        .resizable()
-                        .frame(width: 12, height: 20)
-                        .foregroundStyle(Color.primaryText)
-                        .font(.title.weight(.bold))
-                }
-            }
-            ToolbarItem(placement: .principal) {
-                Text(viewModel.model.texts.title)
-                    .font(Font.customFont(size: 30).weight(.black))
-                    .foregroundStyle(Color.primaryText)
-                    .minimumScaleFactor(0.8)
+            VStack {
+                Spacer()
+                
+                OwnTextField(
+                    text: $viewModel.inputText,
+                    placeholder: viewModel.model.texts.placeholder,
+                    maxCount: viewModel.maximumInputTextCount,
+                    onSubmit: {
+                        viewModel.saveQuestion()
+                    }
+                )
+                .padding(.horizontal, 16)
+                .padding(.vertical, 8)
+                .background(
+                    ZStack {
+                        Colors.ComponentsColors.gameBackground
+                            .ignoresSafeArea()
+                        
+                        CurvedShape()
+                            .ignoresSafeArea()
+                            .foregroundStyle(.curvedShape)
+                    }
+                )
             }
         }
     }
